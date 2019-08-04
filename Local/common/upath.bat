e041f2c7944fde9548c5aa9e1d800749ceefa240
gtr-0000 <1195132772@qq.com>
2019-08-04 08:00:50 +0800

    Rename Local/{exec => common(/bin)}

---
+++ Local/common/upath.bat
@@ -0,0 +1,17 @@
+if not exist save md save
+if not exist save\list.txt break > save\list.txt
+set upath=
+for /f "tokens=1,2 delims=	" %%a in (save\list.txt) do (
+	if "%%b"=="%uname%" (
+		set upath=save\%%a
+	)
+)
+if defined upath exit /b
+for /l %%a in (9999,-1,1) do (
+	if not exist save\%%a (
+		set upath=%%a
+	)
+)
+(echo %upath% %uname%)>save\list.txt
+set upath=save\%upath%
+md %upath%
