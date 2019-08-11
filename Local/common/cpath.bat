87ce17a081c35787190bffb86805b66ab422efa4
gtr-0000 <1195132772@qq.com>
2019-08-11 18:43:30 +0800

    fix

--- Local/common/cpath.bat
+++ Local/common/cpath.bat
@@ -3,15 +3,15 @@ if not exist "%upath%\list.txt" break > "%upath%\list.txt"
 set cpath=
 for /f "usebackq tokens=1,2 delims=	" %%a in ("save\list.txt") do (
 	if "%%b"=="%cname%" (
-		set "cpath=%upath%\%%a"
+		set "cpath=%upath%\%%~a"
 	)
 )
 if defined cpath exit /b
 for /l %%a in (9999,-1,1) do (
 	if not exist "%upath%\%%a" (
-		set cpath=%%a
+		set "cpath=%%~a"
 	)
 )
-(echo %cpath% %uname%)>"save\list.txt"
-set upath=save\%upath%
-md %upath%
+(echo "%cpath%"	%uname%)>"save\list.txt"
+set "cpath=save\%cpath%"
+md "%cpath%"
--- Local/common/upath.bat
+++ Local/common/upath.bat
@@ -2,12 +2,16 @@ if not exist "save" md "save"
 if not exist "save\list.txt" break > "save\list.txt"
 set upath=
 for /f "usebackq tokens=1,2 delims=	" %%a in ("save\list.txt") do (
-	if "%%b"=="%uname%" set "upath=save\%%~a"
+	if "%%b"=="%uname%" (
+		set "upath=save\%%~a"
+	)
 )
 if defined upath exit /b
 for /l %%a in (9999,-1,1) do (
-	if not exist "save\%%a" set "upath=%%~a"
+	if not exist "save\%%a" (
+		set "upath=%%~a"
+	)
 )
-(echo "%upath%" %uname%)>"save\list.txt"
+(echo "%upath%"	%uname%)>"save\list.txt"
 set "upath=save\%upath%"
 md "%upath%"
