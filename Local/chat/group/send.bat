ae48254c60de49ef7fbe9f33bb0d15d0aff22902
gtr-0000 <1195132772@qq.com>
2019-08-13 12:27:19 +0800

    8. chat/group/send

---
+++ Local/chat/group/send.bat
@@ -0,0 +1,13 @@
+@echo off
+2>>"%cpath%\@send.txt" (call :main 3>"%cpath%\#send")
+if not defined text exit /b
+http get "%cpath%\$send" "%server%/chat/group/send.asp" "apikey=%apikey%" "name=%cname:"=%" "text=%text:"=%"
+exit /b
+
+:main
+cls
+tcurs /crv 1 /pos 0 21
+set text=
+set /p text=
+tcurs /crv 0
+cls
